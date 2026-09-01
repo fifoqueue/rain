7825ccd2a861d9d869953e34a138cd42b97ae06c
fifoqueue <me@hsr.vg>
2026-09-01 16:24:08 +0900

    fix: support Discord 343 on iOS

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -79,9 +79,9 @@ const config = {
                                 constModules: {
                                     globals: {
                                         "rain-build-info": {
-                                            version: `"v0.9.7"`,
+                                            version: `"v0.9.8"`,
                                             supportedVersionsAndroid: '337010',
-                                            supportedVersionsIOS: '108502'
+                                            supportedVersionsIOS: '109809'
                                         }
                                     }
                                 },
